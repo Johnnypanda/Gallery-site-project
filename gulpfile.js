81e67f6d0663d4e79bcb8d8e3a4db00da9e24dae
Artem <afilinov93@gmail.com>
2018-01-06 19:48:11 +0100

    Added new modules, responsive images for large-hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,5 @@
-//VARIABLES
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-//DEFAULT TASK
-gulp.task('default', function(){
 
-});
-//HTML TASK
-gulp.task('html', function(){
 
-});
-//OTHER TASKS: POSTCSS PIPE TASKS
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
-
-//WATCH TASKS (GULP-WATCH)
-gulp.task('watch', function(){
-
-	watch('./app/index.html', function(){
-		gulp.start('html');
-	});
-
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
-	});
-});
